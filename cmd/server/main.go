7c3b08871f9d76ac434484485fa599b0cd2d2e61
abdul.hamid1 <abdul.hamid@flip.id>
2023-11-06 14:36:01 +0800

    create main class and start the application

---
+++ cmd/server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func Run() error {
+	fmt.Println("Application run without error")
+	return nil
+}
+func main() {
+	fmt.Println("start the application")
+	if err := Run(); err != nil {
+		fmt.Println("Erro when start application")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/majidehamide/gorest-api2.git
+
+go 1.18
